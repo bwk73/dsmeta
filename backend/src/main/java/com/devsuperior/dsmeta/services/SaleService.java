00c0df0006d711341bcb3380a14258607a665162
Benjamin Wainer Kersz <bwkersz@gmail.com>
2024-01-03 09:48:13 -0300

    Sale listing

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,9 +1,6 @@
 package com.devsuperior.dsmeta.services;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,7 +17,9 @@ public class SaleService {
 	
 	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
 		
-		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		//LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate today = LocalDate.of(2022, 07, 13);
 		
 		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
